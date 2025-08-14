b0ea9b0572395b61c010777b4cc9d80e9a69afd4
khanh <khanh.nguyen.contact@gmail.com>
2025-08-14 22:38:52 +0800

    auto_push

---
+++ bin/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+mkdir -p build
+cd build
+make4ht -d ../docs/build_out/cv ../src/cv/index.tex
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p build
-cd build
-make4ht -d ../docs/assets/cv ../src/cv/index.tex
