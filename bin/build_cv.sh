3818644b898c8004be1b74c8f8f6818429bba356
khanh <khanh.nguyen.contact@gmail.com>
2025-08-14 22:54:52 +0800

    auto_push

--- bin/build_cv.sh
+++ bin/build_cv.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+set -xe
+
 mkdir -p build
 cd build
 make4ht -d ../docs/build_out/cv ../src/cv/main.tex
+cd ..
+rm -rf build
--- bin/copy_latex_styles.sh
+++ bin/copy_latex_styles.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-
 rsync -avh --delete --progress /Users/khanh/mathdoc/latex_styles docs/build_out
