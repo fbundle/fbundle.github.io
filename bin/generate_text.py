487b89e99c55e29f6bd14d032438544fa0250c88
khanh <khanh.nguyen.contact@gmail.com>
2025-08-24 13:26:47 +0800

    auto_push

--- bin/generate_text.py
+++ bin/generate_text.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+from typing import Optional
+
+import pydantic
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import argparse
@@ -10,13 +14,21 @@ from datetime import datetime
 from src.util.pdf_util import get_pdf_dates, get_pdf_text
 from src.util.util import HtmlPath
 
+
 def datetime_to_str(dt: datetime) -> str:
     return dt.strftime("%d %b %Y")
+
+
+class DocDescription(pydantic.BaseModel):
+    summary: str
+    description: dict[str, str]
+
 
 def generate_text_html(
         doc_htmldir: HtmlPath,
         text_template_path: str,
         text_output_path: str,
+        desc_input_path: Optional[str] = None,
 ):
     doc_dir = doc_htmldir.to_path()
     html_template = open(text_template_path).read()
@@ -31,15 +43,24 @@ def generate_text_html(
 
     item_list.sort(key=lambda x: x[2], reverse=True)  # sort by modified date
 
-    description = {}
-    
+    desc = DocDescription(
+        summary="",
+        description={},
+    )
+
+    if desc_input_path is not None:
+        desc = pydantic.BaseModel.model_validate_json(open(desc_input_path).read())
+
     content = ""
+    if len(desc.summary) > 0:
+        content += f"AI summary: {desc.summary}<hr>"
     for name, text_content, creation_date, modified_date in item_list:
         modified_date_str = datetime_to_str(modified_date)
 
         comment = ""
-        if name in description:
-            comment = f'<small style="opacity: 0.6; color: #666; font-style: italic; filter: blur(0.3px);">(AI generated description: {description[name]})</small>'
+        description = desc.description.get(name, "")
+        if len(description) > 0:
+            comment = f'<small style="opacity: 0.6; color: #666; font-style: italic; filter: blur(0.3px);">(AI generated description: {description})</small>'
 
         content += f"""
         <li>
@@ -49,13 +70,11 @@ def generate_text_html(
         </li>
         """
 
-
     html = html_template.format(public_doc_content=content)
     with open(text_output_path, "w") as f:
         f.write(html)
 
     print(f"DEBUG: generated {len(item_list)} entries in {text_output_path}")
-
 
 
 if __name__ == "__main__":
@@ -64,8 +83,8 @@ if __name__ == "__main__":
     parser.add_argument("--doc_htmldir", type=str)
     parser.add_argument("--text_template_path", type=str)
     parser.add_argument("--text_output_path", type=str)
+    parser.add_argument("--desc_input_path", type=Optional[str], default=None)
     args = parser.parse_args()
-
 
     DOC_HTMLDIR = HtmlPath(
         html_root_dir=args.html_root_dir,
@@ -74,6 +93,7 @@ if __name__ == "__main__":
 
     generate_text_html(
         doc_htmldir=DOC_HTMLDIR,
+        desc_input_path=args.desc_input_path,
         text_template_path=args.text_template_path,
         text_output_path=args.text_output_path,
     )
