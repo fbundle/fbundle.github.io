d8afa5490f3b2d5ef93d4d5374cad5ff372f8b85
khanh <khanh.nguyen.contact@gmail.com>
2025-08-24 15:39:18 +0800

    auto_push

--- bin/generate_desc.py
+++ bin/generate_desc.py
@@ -41,8 +41,7 @@ def generate_public_doc_desc(
 
     parts = model.split(":")
     if len(parts) == 1:
-        model_name = parts[0]
-        device_name_list = ["cpu"]
+        model_name, device_name_list = parts[0], ["cpu"]
     else:
         model_name, device_name_list = parts[0], parts[1].split(",")
         device_name_list = [f"cuda:{int(device_name)}" for device_name in device_name_list]
